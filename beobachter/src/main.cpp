35527177aec6262417a093171caff28c0591723e
Jaroslav Matouš <matousss33@gmail.com>
2024-05-10 15:26:13 +0000

    Refactor

--- beobachter/src/main.cpp
+++ beobachter/src/main.cpp
@@ -14,31 +14,39 @@
 
 #define WIFI_SSID "blitzreiter"
 #define WIFI_PSK "Genesis23"
+#define PORT 8686
 
 #define MIN(a, b) (((a) < (b)) ? (a) : (b))
 
-const IPAddress local_ip(192, 168, 1, 1);
-const IPAddress gateway(192, 168, 1, 1);
-const IPAddress subnet(255, 255, 255, 0);
-
-TFT_eSPI tft = TFT_eSPI();
-Joystick joystick = Joystick(PIN_X, PIN_Y);
-
-constexpr unsigned PORT = 8686;
-
-WiFiServer server(PORT);
+// Declarations
 
 void setupScreen();
 void setupWiFi();
 void setupServer();
+void draw(void *);
+void handleVideo(void *);
 
-bool checkHead(byte *);
-void createMessage(byte *);
+// Network
+const IPAddress local_ip(192, 168, 1, 1);
+const IPAddress gateway(192, 168, 1, 1);
+const IPAddress subnet(255, 255, 255, 0);
+
+WiFiServer server(PORT);
 
 const byte messageHead[] = {0xC5, 0x33, 0xFC, 0x33, 0xFC};
 constexpr unsigned messageHeadLen = sizeof(messageHead);
 constexpr unsigned messageLen = messageHeadLen + 2 * sizeof(int);
+byte buffer[16]; // for storing message heads
 
+// Components
+TFT_eSPI tft = TFT_eSPI();
+Joystick joystick = Joystick(PIN_X, PIN_Y);
+
+// Joystick
+constexpr int TREASHOLD = 15;
+std::pair<int, int> lastReading = std::make_pair(0, 0);
+
+// JPEG
 JPEGDEC jpeg;
 struct JPEGBlock
 {
@@ -50,9 +58,119 @@ struct JPEGBlock
 };
 
 JPEGBlock *jpegBlock = nullptr;
-byte buffer[16];
-
 volatile bool doDrawing = false;
+
+void setup()
+{
+  Serial.begin(115200);
+  joystick.init();
+  tft.init();
+  setupScreen();
+  setupWiFi();
+
+  jpegBlock = new JPEGBlock;
+  xTaskCreatePinnedToCore(draw, "draw", 5000, nullptr, 0, nullptr, 0);
+  xTaskCreatePinnedToCore(handleVideo, "handleVideo", 5000, nullptr, 0, nullptr, 1);
+
+  setupServer();
+}
+
+void loop()
+{
+
+  delay(500);
+}
+
+void setupScreen()
+{
+  pinMode(SCREEN_PIN_LED, OUTPUT);
+  digitalWrite(SCREEN_PIN_LED, HIGH);
+
+  // for (int i = 0; i < 3; i++)
+  // {
+  //   tft.fillScreen(TFT_RED);
+  //   delay(1000);
+  //   tft.fillScreen(TFT_GREEN);
+  //   delay(1000);
+  //   tft.fillScreen(TFT_BLUE);
+  //   delay(1000);
+  // }
+
+  tft.setRotation(SCREEN_ORIENTATION);
+  tft.setTextSize(2);
+  tft.setTextColor(TFT_WHITE);
+  tft.fillScreen(TFT_BLACK);
+}
+
+void setupWiFi()
+{
+  WiFi.mode(WIFI_AP);
+  WiFi.softAPConfig(local_ip, gateway, subnet);
+  WiFi.softAP(WIFI_SSID, WIFI_PSK);
+  WiFi.setSleep(false);
+
+  Serial.print("SSID: ");
+  Serial.println(WIFI_SSID);
+  tft.print("SSID: ");
+  tft.println(WIFI_SSID);
+
+  // while (WiFi.status() != WL_CONNECTED)
+  // {
+  //   delay(1000);
+  //   Serial.print('.');
+  //   tft.print('.');
+  // }
+  Serial.println();
+  tft.println();
+}
+
+void setupServer()
+{
+  server.begin();
+  Serial.print("Listening at: ");
+  Serial.print(WiFi.softAPIP());
+  Serial.print(':');
+  Serial.println(PORT);
+}
+
+void createMessage(byte *buffer)
+{
+  std::pair<int, int> xy = joystick.read();
+
+  if (abs(xy.first - lastReading.first) < TREASHOLD && abs(xy.second - lastReading.second) < TREASHOLD)
+  {
+    xy = lastReading;
+  }
+  else
+  {
+    lastReading = xy;
+  }
+  Serial.print("x: ");
+  Serial.print(xy.first);
+  Serial.print(", Y: ");
+  Serial.print(xy.second);
+  Serial.println();
+
+  xy.first = -(xy.first > 0 ? min(xy.first, 2048) : max(xy.first, -2048));
+  xy.second = xy.second > 0 ? min(xy.second, 2048) : max(xy.second, -2048);
+
+  int speed = map(xy.second, -2048, 2048, -255, 255);
+  int steer = map(xy.first, -2048, 2048, -255, 255);
+
+  memcpy(buffer, &messageHead, messageHeadLen);
+  memcpy(buffer + messageHeadLen, &speed, sizeof(int));
+  memcpy(buffer + messageHeadLen + sizeof(int), &steer, sizeof(int));
+}
+
+bool checkHead(byte *buffer)
+{
+  for (unsigned i = 0; i < messageHeadLen; i++)
+  {
+    if (buffer[i] != messageHead[i])
+      return false;
+  }
+  return true;
+}
 
 // jpeg decoding callback, waits for current drawing to end then copies block to `jpegBlock` and enables drawing
 int drawCallback(JPEGDRAW *pDraw)
@@ -92,6 +210,7 @@ void draw(void *)
 void fetchFrame(WiFiClient &client)
 {
   memcpy(buffer, &messageHead, messageHeadLen);
+  // buffer[messageHeadLen] = 1;
   Serial.println("Requesting frame");
   client.write(buffer, messageHeadLen);
   client.flush();
@@ -228,117 +347,4 @@ void handleVideo(void *)
       delay(100);
     }
   }
-}
-
-void setup()
-{
-  Serial.begin(115200);
-  joystick.init();
-  tft.init();
-  setupScreen();
-  setupWiFi();
-  xTaskCreatePinnedToCore(draw, "draw", 5000, nullptr, 0, nullptr, 0);
-  xTaskCreatePinnedToCore(handleVideo, "handleVideo", 5000, nullptr, 0, nullptr, 1);
-  setupServer();
-  jpegBlock = new JPEGBlock;
-}
-
-void loop()
-{
-
-  delay(500);
-}
-
-void setupScreen()
-{
-  pinMode(SCREEN_PIN_LED, OUTPUT);
-  digitalWrite(SCREEN_PIN_LED, HIGH);
-
-  // for (int i = 0; i < 3; i++)
-  // {
-  //   tft.fillScreen(TFT_RED);
-  //   delay(1000);
-  //   tft.fillScreen(TFT_GREEN);
-  //   delay(1000);
-  //   tft.fillScreen(TFT_BLUE);
-  //   delay(1000);
-  // }
-
-  tft.setRotation(SCREEN_ORIENTATION);
-  tft.setTextSize(2);
-  tft.setTextColor(TFT_WHITE);
-  tft.fillScreen(TFT_BLACK);
-}
-
-void setupWiFi()
-{
-  WiFi.mode(WIFI_AP);
-  WiFi.softAPConfig(local_ip, gateway, subnet);
-  WiFi.softAP(WIFI_SSID, WIFI_PSK);
-  WiFi.setSleep(false);
-
-  Serial.print("SSID: ");
-  Serial.println(WIFI_SSID);
-  tft.print("SSID: ");
-  tft.println(WIFI_SSID);
-
-  // while (WiFi.status() != WL_CONNECTED)
-  // {
-  //   delay(1000);
-  //   Serial.print('.');
-  //   tft.print('.');
-  // }
-  Serial.println();
-  tft.println();
-}
-
-void setupServer()
-{
-  server.begin();
-  Serial.print("Listening at: ");
-  Serial.print(WiFi.softAPIP());
-  Serial.print(':');
-  Serial.println(PORT);
-}
-
-constexpr int TREASHOLD = 15;
-std::pair<int, int> lastReading = std::make_pair(0, 0);
-
-void createMessage(byte *buffer)
-{
-  std::pair<int, int> xy = joystick.read();
-
-  if (abs(xy.first - lastReading.first) < TREASHOLD && abs(xy.second - lastReading.second) < TREASHOLD)
-  {
-    xy = lastReading;
-  }
-  else
-  {
-    lastReading = xy;
-  }
-  Serial.print("x: ");
-  Serial.print(xy.first);
-  Serial.print(", Y: ");
-  Serial.print(xy.second);
-  Serial.println();
-
-  xy.first = -(xy.first > 0 ? min(xy.first, 2048) : max(xy.first, -2048));
-  xy.second = xy.second > 0 ? min(xy.second, 2048) : max(xy.second, -2048);
-
-  int speed = map(xy.second, -2048, 2048, -255, 255);
-  int steer = map(xy.first, -2048, 2048, -255, 255);
-
-  memcpy(buffer, &messageHead, messageHeadLen);
-  memcpy(buffer + messageHeadLen, &speed, sizeof(int));
-  memcpy(buffer + messageHeadLen + sizeof(int), &steer, sizeof(int));
-}
-
-bool checkHead(byte *buffer)
-{
-  for (unsigned i = 0; i < messageHeadLen; i++)
-  {
-    if (buffer[i] != messageHead[i])
-      return false;
-  }
-  return true;
 }
